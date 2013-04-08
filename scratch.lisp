99054029c2d7812d0a1cf21d14146897a89ed33e
Aakarsh Nair <aakarsh.nair@gmail.com>
2013-04-08 15:32:51 -0700

    project.lisp

--- scratch.lisp
+++ scratch.lisp
@@ -1,8 +1,31 @@
+(defgeneric draw (shape)
+  (:documentation "Draw a generic shape"))
+
+(defclass circle ()
+  ((radius :initarg :radius :initform 0)))
+
+(defclass point ()
+  ((coordinates :initarg :coordinates :initform '())))
+
+(defclass triangle ()
+  ((point1 :initform :p1)
+   (point2 :initform :p2)
+   (point3 :initform :p3)))
+
+(defmethod draw ((shape circle))
+  (format t "Drawing circle ~%"))
+
+(defmethod draw ((shape triangle))
+  (format t "Drawing triangle ~%"))
+
+
+
+
 (defun find-full-name (name)
   (loop 
      for full-name in  (professor-map->names *professor-map*)
      for last_name = (last-name (string->name full-name))         
-       do 
+     do 
        (if (string= last_name name)
            (return full-name))))
 (defun find-facuty-full-names (last-names)
@@ -20,7 +43,7 @@
            (format t "\"~a\" "  full-name)
            (push name unkown))))
 
-(find-facuty-full-names *ft-faculty*)
+(find-facuty-full-names *ass-faculty*)
 
 
 (defparameter "s ahmed" "s arabhi" "m bodas" "s desousa" "c fan" "t fish" "t huynh" "k jensen" "a jiru" "j jordan" "o kovaleva" "r low" "j lum" "w newball" "l papay" "l sega" "t smith" "a strong" "a talebi" "p tanniru" "j trubey" "j wang" "e zabric" "m zoubeidi" "verga"  "varitanian"  "van der poel"  "a tran"  "rober"  "rigers"  "v nguyen"  "t nguyen"  "a nguyen"  "hillard") 
@@ -35,9 +58,6 @@
 (bfs-pred-path->string (list  (list (car *nodes*) (second *nodes*))))
 (str/join-words (list "hello" "world"))
 
-
-;; (defun enqueue (n q)
-;;   (setf q (push n q)))
 
 (defmacro enqueue (n q)
   `(setf ,q (push ,n ,q)))
